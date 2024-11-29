c3940f1bb571f4a0206cb0a17e61f5f2b829466b
GabrielSGoncalves <gabrielgoncalvesbr@gmail.com>
2024-11-28 23:37:50 -0300

    Unblock public access for bucket

--- project1_aws_static_website/terraform/main.tf
+++ project1_aws_static_website/terraform/main.tf
@@ -5,3 +5,12 @@ resource "aws_s3_bucket" "my_bucket" {
     project = "s3 static website"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
