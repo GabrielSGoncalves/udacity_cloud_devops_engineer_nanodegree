04e6215fd9abcb9df40cf714d4e065842c2ae43b
GabrielSGoncalves <gabrielgoncalvesbr@gmail.com>
2024-12-15 23:56:09 -0300

    Add capabilities parameter to script

--- project2_webapp_with_cloudformation/create_stack.sh
+++ project2_webapp_with_cloudformation/create_stack.sh
@@ -2,5 +2,6 @@ aws cloudformation create-stack --stack-name $1 \
     --template-body file://$2   \
     --parameters file://$3  \
     --region=us-east-1  \
+    --capabilities CAPABILITY_IAM \
     --profile udacity
 
