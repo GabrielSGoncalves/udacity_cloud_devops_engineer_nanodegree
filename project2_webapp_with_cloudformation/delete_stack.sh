f2c8c6c6bb1d98f0fdcab05d8b70afbf2f0f8baf
GabrielSGoncalves <gabrielgoncalvesbr@gmail.com>
2024-12-15 23:56:33 -0300

    Delete useless block

--- project2_webapp_with_cloudformation/delete_stack.sh
+++ project2_webapp_with_cloudformation/delete_stack.sh
@@ -1,7 +1,3 @@
 aws cloudformation delete-stack \
    --stack-name $1 \
-   --region us-east-1 
-
-   aws cloudformation delete-stack \
-   --stack-name first-test-stack \
-   --region us-east-1 
+   --region us-east-1
