3bde9cc77fe42e3bfd9f76a3cd06ae74ec77800f
GabrielSGoncalves <gabrielgoncalvesbr@gmail.com>
2025-02-19 23:09:50 -0300

    Remove unused files

--- project3_microservices_with_kubernetes/terrafom/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "test-bucket-123-2024-12-28"
-
-  tags = {
-    Name        = "Test Bucket"
-    Environment = "Development"
-  }
-}
--- project4_cicd_with_github_actions/README.md
+++
@@ -1,9 +0,0 @@
-# Project 4 - CI/CD with Github Actions
-## Overview
-
-
-## Objective
-1. 
-
-
-## Final thoughts
